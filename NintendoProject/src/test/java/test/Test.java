73a791813eae4f1c4bd86604c96f3515c5ac504c
NicolasJoseph <Joseph_nicolas@outlook.fr>
2023-01-11 10:49:34 +0100

    main fix

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -4,6 +4,7 @@ import model.Adresse;
 import model.Boutique;
 import model.Client;
 import model.Console;
+import model.Hybride;
 import model.Jeu;
 
 public class Test {
@@ -12,13 +13,13 @@ public class Test {
 		
 		Adresse a1 = new Adresse ("1","rue de Paris","Paris");
 		Boutique b1 = new Boutique("MaBoutique",a1);
-		Console c1= new Console("Switch");
+		Console c1= new Hybride("Switch");
 		
-		Jeu j1= new Jeu("Mario Kart",c1);
-		Jeu j2= new Jeu("Animal crossing",c1);
-		Jeu j3= new Jeu("Just dance",c1);
-		Jeu j4= new Jeu("Splatoon",c1);
-		Jeu j5 = new Jeu("Pikmin",c1);
+		Jeu j1= new Jeu("Mario Kart",c1,b1);
+		Jeu j2= new Jeu("Animal crossing",c1,b1);
+		Jeu j3= new Jeu("Just dance",c1,b1);
+		Jeu j4= new Jeu("Splatoon",c1,b1);
+		Jeu j5 = new Jeu("Pikmin",c1,b1);
 
 		Client client1 = new Client("Abid","Jordan");
 		Client client2 = new Client("Doe","John");
