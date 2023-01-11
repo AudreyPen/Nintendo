314346c3714b86ca6d0f182e4ef70fe999815c78
NicolasJoseph <Joseph_nicolas@outlook.fr>
2023-01-11 10:20:45 +0100

    Add two client

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,9 +1,13 @@
 package test;
 
+import model.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+
+		Client client1 = new Client("Abid","Jordan");
+		Client client2 = new Client("Doe","John");
 
 	}
 
