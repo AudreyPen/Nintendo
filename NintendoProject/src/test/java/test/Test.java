35f554cffc5bd14aa8e6d4f3be610b80fd92c024
audreypen <audreypenazzi@gmail.com>
2023-01-11 10:26:06 +0100

    Ajout objet jeu et console main

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -3,12 +3,20 @@ package test;
 import model.Adresse;
 import model.Boutique;
 
+
 public class Test {
 
 	public static void main(String[] args) {
 		
 		Adresse a1 = new Adresse ("1","rue de Paris","Paris");
 		Boutique b1 = new Boutique("MaBoutique",a1);
+		Console c1= New Console ("Switch");
+		
+		Jeu j1= New Jeu ("Mario Kart",c1);
+		Jeu j2= New Jeu ("Animal crossing",c1);
+		Jeu j3= New Jeu ("Just dance",c1);
+		Jeu j4= New Jeu ("Splatoon",c1);
+		Jeu j5 = New jeu ("Pikmin",c1);
 
 	}
 
