93d0c2602c512e1172f1a238063332513eb7dc32
NicolasJoseph <Joseph_nicolas@outlook.fr>
2023-01-11 11:01:35 +0100

    Add listeAchat on client

--- NintendoProject/src/main/java/model/Hybride.java
+++ NintendoProject/src/main/java/model/Hybride.java
@@ -1,9 +1,13 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Hybride extends Console {
-	
-	public Hybride(String nom) {
-		super(nom);
+
+	public Hybride(String nom, Double prix, LocalDate date) {
+		super(nom, prix, date);
 	}
+	
+	
 
 }
